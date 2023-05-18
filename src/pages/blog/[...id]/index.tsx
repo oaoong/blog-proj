9cd5ffc173410880577b47e417df6db5a4c197b3
oaoong <pizzachicken@kakao.com>
2023-05-19 00:38:22 +0900

    :tada: make mdx file print on post component

---
+++ src/pages/blog/[...id]/index.tsx
@@ -0,0 +1,10 @@
+import BlogPost from '@/templates/blog/blog-post'
+import React from 'react'
+
+export default function Post() {
+  return (
+    <div className='w-full h-full'>
+      <BlogPost />
+    </div>
+  )
+}
