6b1f167c15b591b9dcf98a1afb77be72030cd057
oaoong <pizzachicken@kakao.com>
2023-10-05 18:13:00 +0900

    :sparkles: 이미지 변경

--- src/pages/api/og-image.tsx
+++ src/pages/api/og-image.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from '@vercel/og'
 
 export const config = {
@@ -6,33 +5,32 @@ export const config = {
 }
 
 export default async function handler() {
-  const imageData = await fetch(
-    new URL('./thumbnail.png', import.meta.url),
-  ).then((res) => res.arrayBuffer())
   return new ImageResponse(
     (
       <div
         style={{
           display: 'flex',
+          fontSize: 60,
+          color: 'black',
           background: '#f6f6f6',
           width: '100%',
           height: '100%',
+          paddingTop: 50,
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        <img
-          width='256'
-          height='256'
-          src={imageData as unknown as string}
-          alt={'thumbnail'}
-        />
+        👋, 🌎
+        <p>Jay Log입니다</p>
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      // Supported options: 'twemoji', 'blobmoji', 'noto', 'openmoji', 'fluent' and 'fluentFlat'
+      // Default to 'twemoji'
+      emoji: 'twemoji',
     },
   )
 }
